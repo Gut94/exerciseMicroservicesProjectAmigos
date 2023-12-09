603c83c792113a7e20bb6d1b16af0bbc2602e45e
Gut_PC <raulnlh@gmail.com>
2023-12-09 13:33:00 +0100

    fraud branch

--- gutservice/customer/src/main/java/com/gut/customer/CustomerController.java
+++ gutservice/customer/src/main/java/com/gut/customer/CustomerController.java
@@ -20,6 +20,7 @@ public record CustomerController(CustomerService customerService) {	//record es 
 	public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
 		log.info("new customer registration{}",customerRegistrationRequest);//instalar lombok run as app en dependencias y restart y  clean e lproyecto
 		customerService.registerCustomer(customerRegistrationRequest);
+		//de post vienen los datos que son los mismos que customerRegistrationRequest y los guarda en bbdd con customerService.registerCustomer
 
 	}
 
